57e33d2972b5d1bc0486e31a13b60b07f95a86a0
Etika Khullar <khullaretika@gmail.com>
2026-03-06 10:07:22 +0530

    Factory Design Pattern

---
+++ src/main/java/com/assignment/question/DocumentProcessor.java
@@ -0,0 +1,14 @@
+package com.assignment.question;
+
+public abstract class DocumentProcessor {
+    private String documentName;
+   
+    public  DocumentProcessor(String documentName) {
+        this.documentName = documentName;
+    }
+    public String getDocumentName() {
+        return documentName;
+    }
+    public abstract void processDocument();
+    public abstract DocumentType supportsType();
+}
